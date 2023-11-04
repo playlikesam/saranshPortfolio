9dbc05c994526246559e75eada5487ea4a7a32b8
Saransh Mishra <saranshmishramishra@gmail.com>
2023-11-04 21:33:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing Animation
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer","Graphics Designer","Content Writer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
