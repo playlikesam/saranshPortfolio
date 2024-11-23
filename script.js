40e52b365850ab892899a2a9b85146b7eed00e36
Saransh Mishra <saranshmishramishra@gmail.com>
2024-11-23 21:21:21 +0530

    updated the website

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // typing Animation
 var typed = new Typed(".typing",{
-    strings:["Web Designer","Web Developer","Graphics Designer","Content Writer"],
+    strings:["Developer","Designer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
